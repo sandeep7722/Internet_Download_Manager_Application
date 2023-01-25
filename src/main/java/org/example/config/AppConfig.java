22d12fef81142f233a4f1dadc07c74e4dcbd0666
sandeep <sandeepsaroj167@gmail.com>
2023-01-25 09:15:39 +0530

    first commit

---
+++ src/main/java/org/example/config/AppConfig.java
@@ -0,0 +1,7 @@
+package org.example.config;
+
+public class AppConfig {
+    public static final String DOWNLOAD_PATH="C:\\Users\\sande\\Desktop\\samir\\development";
+    //other config information
+
+}
